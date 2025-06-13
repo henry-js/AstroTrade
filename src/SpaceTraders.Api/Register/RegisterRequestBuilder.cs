343e08f430e70dfe92c3e3c4b4969a814807bd17
James Henry <james.henry@kerridgecs.com>
2025-06-13 13:25:08 +0100

    chore: regenerate client

--- src/SpaceTraders.Api/Register/RegisterRequestBuilder.cs
+++ src/SpaceTraders.Api/Register/RegisterRequestBuilder.cs
@@ -39,6 +39,7 @@ namespace SpaceTraders.Api.Register
         /// <param name="body">The request body</param>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
+        /// <exception cref="global::SpaceTraders.Api.Register.Register409Error">When receiving a 409 status code</exception>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
         public async Task<global::SpaceTraders.Api.Register.RegisterPostResponse?> PostAsRegisterPostResponseAsync(global::SpaceTraders.Api.Register.RegisterPostRequestBody body, Action<RequestConfiguration<DefaultQueryParameters>>? requestConfiguration = default, CancellationToken cancellationToken = default)
@@ -50,7 +51,11 @@ namespace SpaceTraders.Api.Register
 #endif
             _ = body ?? throw new ArgumentNullException(nameof(body));
             var requestInfo = ToPostRequestInformation(body, requestConfiguration);
-            return await RequestAdapter.SendAsync<global::SpaceTraders.Api.Register.RegisterPostResponse>(requestInfo, global::SpaceTraders.Api.Register.RegisterPostResponse.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
+            var errorMapping = new Dictionary<string, ParsableFactory<IParsable>>
+            {
+                { "409", global::SpaceTraders.Api.Register.Register409Error.CreateFromDiscriminatorValue },
+            };
+            return await RequestAdapter.SendAsync<global::SpaceTraders.Api.Register.RegisterPostResponse>(requestInfo, global::SpaceTraders.Api.Register.RegisterPostResponse.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Creates a new agent and ties it to an account. The agent symbol must consist of a 3-14 character string, and will be used to represent your agent. This symbol will prefix the symbol of every ship you own. Agent symbols will be cast to all uppercase characters.This new agent will be tied to a starting faction of your choice, which determines your starting location, and will be granted an authorization token, a contract with their starting faction, a command ship that can fly across space with advanced capabilities, a small probe ship that can be used for reconnaissance, and 175,000 credits.&gt; #### Keep your token safe and secure&gt;&gt; Keep careful track of where you store your token. You can generate a new token from our account dashboard, but if someone else gains access to your token they will be able to use it to make API requests on your behalf until the end of the reset.If you are new to SpaceTraders, It is recommended to register with the COSMIC faction, a faction that is well connected to the rest of the universe. After registering, you should try our interactive [quickstart guide](https://docs.spacetraders.io/quickstart/new-game) which will walk you through a few basic API requests in just a few minutes.
@@ -59,6 +64,7 @@ namespace SpaceTraders.Api.Register
         /// <param name="body">The request body</param>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
+        /// <exception cref="global::SpaceTraders.Api.Register.Register409Error">When receiving a 409 status code</exception>
         [Obsolete("This method is obsolete. Use PostAsRegisterPostResponseAsync instead.")]
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -71,7 +77,11 @@ namespace SpaceTraders.Api.Register
 #endif
             _ = body ?? throw new ArgumentNullException(nameof(body));
             var requestInfo = ToPostRequestInformation(body, requestConfiguration);
-            return await RequestAdapter.SendAsync<global::SpaceTraders.Api.Register.RegisterResponse>(requestInfo, global::SpaceTraders.Api.Register.RegisterResponse.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
+            var errorMapping = new Dictionary<string, ParsableFactory<IParsable>>
+            {
+                { "409", global::SpaceTraders.Api.Register.Register409Error.CreateFromDiscriminatorValue },
+            };
+            return await RequestAdapter.SendAsync<global::SpaceTraders.Api.Register.RegisterResponse>(requestInfo, global::SpaceTraders.Api.Register.RegisterResponse.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
         /// Creates a new agent and ties it to an account. The agent symbol must consist of a 3-14 character string, and will be used to represent your agent. This symbol will prefix the symbol of every ship you own. Agent symbols will be cast to all uppercase characters.This new agent will be tied to a starting faction of your choice, which determines your starting location, and will be granted an authorization token, a contract with their starting faction, a command ship that can fly across space with advanced capabilities, a small probe ship that can be used for reconnaissance, and 175,000 credits.&gt; #### Keep your token safe and secure&gt;&gt; Keep careful track of where you store your token. You can generate a new token from our account dashboard, but if someone else gains access to your token they will be able to use it to make API requests on your behalf until the end of the reset.If you are new to SpaceTraders, It is recommended to register with the COSMIC faction, a faction that is well connected to the rest of the universe. After registering, you should try our interactive [quickstart guide](https://docs.spacetraders.io/quickstart/new-game) which will walk you through a few basic API requests in just a few minutes.
